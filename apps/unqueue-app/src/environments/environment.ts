efed0037d03d9149703c44059aaa8c56f22379c1
Rafael E. Ajuria <reajuria@twinhz.com>
2020-12-06 11:21:59 -0600

    Added Firebase and Environment Config

--- apps/unqueue-app/src/environments/environment.ts
+++ apps/unqueue-app/src/environments/environment.ts
@@ -1,6 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 // This file can be replaced during build by using the `fileReplacements` array.
 // When building for production, this file is replaced with `environment.prod.ts`.
 
+const devEnvironment = require('../../../../environment.json');
+
 export const environment = {
   production: false,
+  firebaseCredentials: devEnvironment.firebase,
 };
--- apps/unqueue-app/src/main.tsx
+++ apps/unqueue-app/src/main.tsx
@@ -1,9 +1,11 @@
+import firebase from 'firebase/app';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { BrowserRouter } from 'react-router-dom';
-
 import App from './app/app';
+import { environment } from './environments/environment';
+
+firebase.initializeApp(environment.firebaseCredentials);
 
 ReactDOM.render(
   <React.StrictMode>
